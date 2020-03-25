609c798243cfaa707d8877b35e8723d01219f859
Sabbir <sabbirahmedcse33@gmail.com>
2020-03-25 21:57:27 +0600

    Animal class Complete.

--- src/Oop/Animal.java
+++ src/Oop/Animal.java
@@ -1,32 +1,22 @@
 package Oop;
 
-/**
- * @author arafat
- */
+import java.util.Scanner;
+
+
 public class Animal {
      public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int test=sc.nextInt();
-        
-        int i=0;
-        
-        while(i<test ){
-            int x=sc.nextInt();
-            int y=sc.nextInt();
-            int z=sc.nextInt();
-            
-            if(Math.abs(x - z) < Math.abs(y-z)){
-            System.out.println("Cat A");
-            }
-            else if (Math.abs(x - z) > Math.abs(y-z)){
-            System.out.println("Cat B");
-            }
-            else{
-            System.out.println("Mouse C");
-           
-            }
-          ++i;   
+         eat();
+         sleep();
+
     }
-  }
+
+    public static void eat(){
+        System.out.println("Eating");
+    }
+
+    public static void sleep(){
+        System.out.println("Sleeping");
+    }
+
     
 }
--- src/Oop/ObjectExample.java
+++ src/Oop/ObjectExample.java
@@ -2,35 +2,9 @@ package Oop;
 
 import java.util.Scanner;
 
-/**
- *
- * @author arafat
- */
+
 
 public class ObjectExample {
-        public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int test=sc.nextInt();
-        
-        int i=0;
-        
-        while(i<test ){
-            int x=sc.nextInt();
-            int y=sc.nextInt();
-            int z=sc.nextInt();
-            
-            if(Math.abs(x - z) < Math.abs(y-z)){
-            System.out.println("Cat A");
-            }
-            else if (Math.abs(x - z) > Math.abs(y-z)){
-            System.out.println("Cat B");
-            }
-            else{
-            System.out.println("Mouse C");
-           
-            }
-          ++i;   
-    }
-  }
+
 }
     
