d4560945dc8df15e64911f8993437c514b86b180
Sabbir <sabbirahmedcse33@gmail.com>
2020-03-25 22:04:04 +0600

    static class practice.

--- src/Oop/Animal.java
+++ src/Oop/Animal.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 
 public class Animal {
-     public static void main(String[] args) {
-         eat();
-         sleep();
-
-    }
 
     public static void eat(){
         System.out.println("Eating");
---
+++ src/Oop/Main.java
@@ -0,0 +1,9 @@
+package Oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal.eat();
+        Animal.sleep();
+
+    }
+}
